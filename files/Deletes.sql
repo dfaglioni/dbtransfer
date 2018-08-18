3495163797363887564c3b3b9e6a89f8806639f1
Rodolfo Jordão <rodolfo.bacini@elotech.com.br>
2018-08-18 10:29:13 -0300

    Deletes e tipoLogradouro

---
+++ files/Deletes.sql
@@ -0,0 +1,24 @@
+delete from MODELOARQUIVO where modulo = '05';
+delete from MODELOARQUIVOATRIBUTO where modulo = '05';
+delete from MODELOARQUIVOCAMPO where modulo = '05';
+delete from MODELOARQUIVOESTRUTURA where modulo = '05';
+delete from MODELOARQUIVOGRUPO where modulo = '05';
+delete from MODELOARQUIVOGRUPOMODELO where modulo = '05';
+delete from MODULOATUALIZACAO where modulo = '05';
+
+delete from ROTINAARQUIVO where modulo = '05';
+
+delete from PARAMETROENTIDADE where modulo = '05';
+delete from PARAMETROENTIDADEEXERCICIO where modulo = '05';
+delete from PARAMETROGERAL where modulo = '05';
+
+delete from SELETOR where modulo = '05';
+delete from SELETORCAMPO where modulo = '05';
+
+delete from USUARIOMODULO where modulo = '05';
+delete from USUARIOSELETOR where modulo = '05';
+delete from USUARIOSELETORCAMPO where modulo = '05';
+delete from USUARIOSELETORCFG where modulo = '05';
+delete from USUARIOSELETORGERAL where modulo = '05';
+
+
